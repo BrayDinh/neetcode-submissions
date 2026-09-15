1c0da1b0f3f937d49d390cd2ec83f3473deda694
BrayDinh <braydendinh1234@gmail.com>
2026-09-15 17:42:29 -0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = "";
+
+        int tracker = 0;
+
+        for (int i = str.length()-1; i >= 0; i--){
+            reversed += str.charAt(i);
+        }
+
+        if (reversed.equals(str)) return true;
+        return false;
+        
+    }
+}
